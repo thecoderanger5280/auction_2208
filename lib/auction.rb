abb7da58e14163041a124d79d00b6d95e5257435
Ryan Loughry <thecoderanger5280@icloud.com>
2022-09-26 10:34:56 -0600

    implement code for bidder info in auction class

--- lib/auction.rb
+++ lib/auction.rb
@@ -31,4 +31,17 @@ class Auction
     end
     bidders.flatten.uniq.map(&:name)
   end
+
+  def bidder_info
+    info = {}
+    bid_items = @items.find_all { |item| item.bids != {}}
+    bidders = bid_items.map do |item|
+      item.bids.keys
+    end
+    bidders.flatten.uniq.each do |bidder|
+      # require 'pry'; binding.pry
+      info[bidder] = {budget: bidder.budget, items: bid_items.find_all { |item| item.bids.keys.include?(bidder)}}
+    end
+    info
+  end
 end
