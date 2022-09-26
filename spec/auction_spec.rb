f95de79cbde0d202bbac07af31128006b35bd6bb
Ryan Loughry <thecoderanger5280@icloud.com>
2022-09-26 10:18:24 -0600

    write test for bidder_info in auction class

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -124,4 +124,43 @@ RSpec.describe Auction do
       expect(auction.bidders).to eq(['Megan', 'Bob', 'Mike'])
     end
   end
+
+  describe '#bidder_info' do
+    it 'can return the bidder info' do
+      auction = Auction.new
+      item1 = Item.new('Chalkware Piggy Bank')
+      item2 = Item.new('Bamboo Picture Frame')
+      item3 = Item.new('Homemade Chocolate Chip Cookies')
+      item4 = Item.new('2 Days Dogsitting')
+      item5 = Item.new('Forever Stamps')
+      attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+      attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+      attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+
+      item1.add_bid(attendee1, 22)
+      item1.add_bid(attendee2, 20)
+      item4.add_bid(attendee3, 50)
+      item3.add_bid(attendee2, 15)
+
+      expect(auction.bidder_info).to eq({
+        attendee1 => {
+          budget: 50,
+          items: [item1]
+        },
+        attendee2 => {
+          budget: 75,
+          items: [item1, item3]
+        },
+        attendee3 => {
+          budget: 100,
+          items: [item4]
+        }
+      })
+    end
+  end
 end
