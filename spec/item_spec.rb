47004b04f11d8a42aeb1cf8dc71fd38121a3a77d
Ryan Loughry <thecoderanger5280@icloud.com>
2022-09-26 10:12:40 -0600

    write test for close bidding in item class

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,6 +1,7 @@
 require 'rspec'
 require './lib/item'
 require './lib/attendee'
+require './lib/auction'
 
 RSpec.describe Item do
   describe '#initialize' do
@@ -46,6 +47,38 @@ RSpec.describe Item do
       item1.add_bid(attendee1, 22)
 
       expect(item1.current_high_bid).to eq(22)
+    end
+  end
+
+  describe '#close_bidding' do
+    it 'can close the bidding so nobody can bid anymore' do
+      auction = Auction.new
+      item1 = Item.new('Chalkware Piggy Bank')
+      item2 = Item.new('Bamboo Picture Frame')
+      item3 = Item.new('Homemade Chocolate Chip Cookies')
+      item4 = Item.new('2 Days Dogsitting')
+      item5 = Item.new('Forever Stamps')
+      attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+      attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+      attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+
+      item1.add_bid(attendee1, 22)
+      item1.add_bid(attendee2, 20)
+      item4.add_bid(attendee3, 50)
+      item3.add_bid(attendee2, 15)
+
+      expect(item1.bids).to eq({attendee1 => 22, attendee2 => 20})
+
+      item1.close_bidding
+
+      item1.add_bid(attendee3, 70)
+
+      expect(item1.bids).to eq({attendee1 => 22, attendee2 => 20})
     end
   end
 end
