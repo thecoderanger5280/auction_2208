46b9e6202a5fac13d15a4af5c4151a10d6a0e8d5
Ryan Loughry <thecoderanger5280@icloud.com>
2022-09-26 09:24:54 -0600

    write test for the item names block in auction class

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -29,4 +29,17 @@ RSpec.describe Auction do
       expect(auction.items).to eq([item1, item2])
     end
   end
+
+  describe '#item_names' do
+    it 'can get the item names' do
+      auction = Auction.new
+      item1 = Item.new('Chalkware Piggy Bank')
+      item2 = Item.new('Bamboo Picture Frame')
+
+      auction.add_item(item1)
+      auction.add_item(item2)
+
+      expect(auction.item_names).to eq(['Chalkware Piggy Bank', 'Bamboo Picture Frame'])
+    end
+  end
 end
