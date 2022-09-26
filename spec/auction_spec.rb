32721c4e9fadd2153299f12e505c008add7a98f3
Ryan Loughry <thecoderanger5280@icloud.com>
2022-09-26 09:22:56 -0600

    write test for the add items block  on auction class

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -16,4 +16,17 @@ RSpec.describe Auction do
       expect(auction.items).to eq([])
     end
   end
+
+  describe '#add_item' do
+    it 'can add items' do
+      auction = Auction.new
+      item1 = Item.new('Chalkware Piggy Bank')
+      item2 = Item.new('Bamboo Picture Frame')
+
+      auction.add_item(item1)
+      auction.add_item(item2)
+
+      expect(auction.items).to eq([item1, item2])
+    end
+  end
 end
