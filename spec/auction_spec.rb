9719c6302dbae609a97e01d80298391c5cc36533
Ryan Loughry <thecoderanger5280@icloud.com>
2022-09-26 09:48:53 -0600

    write test for unpopular items in auction class

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,6 +1,7 @@
 require 'rspec'
 require './lib/item'
 require './lib/auction'
+require './lib/attendee'
 
 RSpec.describe Auction do
   describe '#initialize' do
@@ -40,6 +41,31 @@ RSpec.describe Auction do
       auction.add_item(item2)
 
       expect(auction.item_names).to eq(['Chalkware Piggy Bank', 'Bamboo Picture Frame'])
+    end
+  end
+
+  describe '#unpopular_items' do
+    it 'can get a list of the items that dont have bids' do
+      auction = Auction.new
+      item1 = Item.new('Chalkware Piggy Bank')
+      item2 = Item.new('Bamboo Picture Frame')
+      item3 = Item.new('Homemade Chocolate Chip Cookies')
+      item4 = Item.new('2 Days Dogsitting')
+      item5 = Item.new('Forever Stamps')
+      attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+      attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+      attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+
+      item1.add_bid(attendee2, 20)
+      item1.add_bid(attendee1, 22)
+      item4.add_bid(attendee3, 50)
+
+      expect(auction.unpopular_items).to eq([item2, item3, item5])
     end
   end
 end
